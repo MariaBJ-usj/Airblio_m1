b74cb7e3a5f21a6847a934bf6ea2f081b0ae1db4
maria bou jawde <maria.boujaoude@net.usj.edu.lb>
2022-04-10 16:28:15 +0200

    edited master page, added new content to accueil

--- resources/views/acceuil.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('default')
-
-@section('page-content')
-    
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('acceuil');
-})->name('acceuil');
+    return view('accueil');
+})->name('accueil');
 
 Route::get('/indentification', function () {
     return view('indentification');
